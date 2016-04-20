ab13ebef0bda92e5726bffd183cd597956020b18
thejinchao <thejinchao@gmail.com>
2016-04-20 10:20:36 +0800

    添加404页面，调整部分页面设置

---
+++ 404.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 404 template.
+ */
+
+get_header(); ?>
+
+	<div id="container">
+		<div id="content" role="main">
+
+			<div id="error404">
+			
+			</div><!-- #post-0 -->
+
+		</div><!-- #content -->
+	</div><!-- #container -->
+	<script type="text/javascript">
+		document.getElementById('s') && document.getElementById('s').focus();
+	</script>
+
+<?php get_footer(); ?>
